f5c94be81b98e5c43c47647f38016fce2e807497
gfrancine <gracefrancine.t@gmail.com>
2021-07-01 23:32:11 +0700

    split menubar to left and right

--- mnote/modules/menubar.ts
+++ mnote/modules/menubar.ts
@@ -7,17 +7,35 @@ import { el } from "../common/elbuilder";
 
 export class MenubarModule /* implements Module */ {
   element: HTMLElement;
+  left: HTMLElement;
+  right: HTMLElement;
   app: Mnote;
 
   constructor(app: Mnote) {
     this.app = app;
+
+    this.left = el("div")
+      .class("menubar-left")
+      .element;
+
+    this.right = el("div")
+      .class("menubar-right")
+      .element;
+
     this.element = el("div")
       .class("menubar")
+      .children(
+        this.left,
+        this.right,
+      )
       .element;
+
     (app.modules.layout as LayoutModule).mountToMenubar(this.element);
     console.log(this.element.parentNode);
   }
 
   setMenubarButtons(buttons: MenubarButton[]) {
   }
+
+  setMenubarText(text: string) {}
 }
