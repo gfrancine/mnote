bc4755d52f25a7927e201526109519e432a0fb09
gfrancine <gracefrancine.t@gmail.com>
2021-07-06 14:26:21 +0700

    setup cli args (not working)

---
+++ app/run.bat
@@ -0,0 +1,1 @@
+src-tauri\target\debug\Mnote "tauri.txt dest.txt"
--- app/src/index.ts
+++ app/src/index.ts
@@ -1,8 +1,17 @@
 import { EditorsModule, Mnote } from "../../mnote";
 import { FS } from "./fs";
 import "../../mnote-styles/light.scss";
+import * as cli from "@tauri-apps/api/cli";
+cli.getMatches().then(console.log);
+
+console.log("hey");
 
 (async () => {
+  console.log("YoO");
+  const matches = await cli.getMatches();
+  let startPath: string | undefined;
+  console.log("YO", matches);
+
   const app = new Mnote("#root", {
     startPath: "D:\\Notes\\written\\sample.md",
     fs: new FS(),
