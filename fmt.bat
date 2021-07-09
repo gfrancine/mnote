821926e42bbebc6c5e2433f6f779bc424db02572
gfrancine <gracefrancine.t@gmail.com>
2021-07-09 17:52:30 +0700

    rename app to mnote-app

--- fmt.bat
+++ fmt.bat
@@ -1,5 +1,5 @@
 deno fmt mnote-core/src
 deno fmt mnote-extensions/markdown
-deno fmt app/src 
-deno fmt app/src-web
-cd app/src-tauri && cargo fmt && cd ../..
+deno fmt mnote-app/src 
+deno fmt mnote-app/src-web
+cd mnote-app/src-tauri && cargo fmt && cd ../..
