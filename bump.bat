6f7d56f0b5c7c4428cc43af100e7f78c916c6e13
gfrancine <gracefrancine.t@gmail.com>
2021-07-11 21:44:00 +0700

    use powershell install script

--- bump.bat
+++ bump.bat
@@ -8,10 +8,4 @@ cd ../mnote-core && rmdir /s /q node_modules
 cd ../mnote-app && rmdir /s /q node_modules
 cd ..
 
-npm i^
-  && cd mnote-util && npm i^
-  && cd ../mnote-styles && npm i^
-  && cd ../mnote-extensions && npm i^
-  && cd ../mnote-core && npm i^
-  && cd ../mnote-app && npm i^
-  && cd ..
+powershell .\install.ps1
