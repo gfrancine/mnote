68b7e423c796c1177060178e0798752361235399
gfrancine <gracefrancine.t@gmail.com>
2021-07-16 23:29:16 +0700

    downgrade to npm 6

--- install.ps1
+++ install.ps1
@@ -1,7 +1,12 @@
-npm i `
-  && cd mnote-util && npm i `
-  && cd ../mnote-styles && npm i `
-  && cd ../mnote-extensions && npm i `
-  && cd ../mnote-core && npm i `
-  && cd ../mnote-app && npm i `
-  && cd ..
+npm i
+cd mnote-util
+npm i
+cd ../mnote-styles
+npm i
+cd ../mnote-extensions
+npm i
+cd ../mnote-core
+npm i
+cd ../mnote-app
+npm i
+cd ..
