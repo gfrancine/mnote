b5bf3f05dc5ba0df0974b116f1faefa97173441c
gfrancine <gracefrancine.t@gmail.com>
2021-07-16 16:10:05 +0700

    fix icon hover, icon alt, format

--- mnote-core/src/components/icons.ts
+++ mnote-core/src/components/icons.ts
@@ -21,7 +21,6 @@ const icons = {
   ) =>
     toHtml(`
     <svg viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg">
-      <title>ionicons-v5-f</title>
       <circle cx="256" cy="256" r="32" class="${strokeClass}" style="fill:none;stroke-miterlimit:10;stroke-width:32px"/>
       <circle cx="256" cy="416" r="32" class="${strokeClass}" style="fill:none;stroke-miterlimit:10;stroke-width:32px"/>
       <circle cx="256" cy="96" r="32" class="${strokeClass}" style="fill:none;stroke-miterlimit:10;stroke-width:32px"/>
