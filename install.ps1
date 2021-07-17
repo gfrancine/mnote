83308ffb87359951c2c88f03b7966a5f4e04c9e6
gfrancine <gracefrancine.t@gmail.com>
2021-07-17 21:08:49 +0700

    revert npm downgrade

--- install.ps1
+++ install.ps1
@@ -1,13 +1,14 @@
 npm --version
-Get-ChildItem -Path "." "package-lock.json" -Recurse | foreach { Remove-Item -Path $_.FullName }
 
-npm i
+# Get-ChildItem -Path "." "package-lock.json" -Recurse | foreach { Remove-Item -Path $_.FullName }
+
+npm i --legacy-peer-deps
 cd mnote-util
 npm i
 cd ../mnote-styles
 npm i
 cd ../mnote-extensions
-npm i
+npm i --legacy-peer-deps
 cd ../mnote-core
 npm i
 cd ../mnote-app
