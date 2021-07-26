1f952d55ece2e21e5ac9812312e5f93e6f3d1c42
Grace <gracefrancine.t@gmail.com>
2021-07-26 04:07:38 +0000

    add workspace script

---
+++ workspace.sh
@@ -0,0 +1,20 @@
+# install powershell
+cd ..
+sudo apt-get update
+sudo apt-get install -y wget apt-transport-https software-properties-common
+wget -q https://packages.microsoft.com/config/ubuntu/16.04/packages-microsoft-prod.deb
+sudo dpkg -i packages-microsoft-prod.deb
+sudo apt-get update
+sudo apt-get install -y powershell
+
+# install deno
+curl -fsSL https://deno.land/x/install/install.sh | sh
+export DENO_INSTALL="/home/gitpod/.deno"
+export PATH="$DENO_INSTALL/bin:$PATH"
+
+# install dependencies
+cd mnote
+npm i -g npm@7
+npm --version
+pwsh .\\install.ps1
+git reset --hard
