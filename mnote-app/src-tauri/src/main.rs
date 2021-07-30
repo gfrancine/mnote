b9da217419cbe3802add40489a8402c9e1b8399e
gfrancine <gracefrancine.t@gmail.com>
2021-07-30 21:47:09 +0700

    add menu for macos

--- mnote-app/src-tauri/src/main.rs
+++ mnote-app/src-tauri/src/main.rs
@@ -18,6 +18,10 @@ fn is_windows() -> bool {
   cfg!(windows)
 }
 
+use notify::{watcher, RecursiveMode, Watcher};
+use std::sync::mpsc::channel;
+use std::time::Duration;
+
 #[tauri::command]
 async fn watcher_init(window: tauri::Window, path: String) {
   let (tx, rx) = channel();
@@ -32,13 +36,49 @@ async fn watcher_init(window: tauri::Window, path: String) {
   }
 }
 
-use notify::{watcher, RecursiveMode, Watcher};
-use std::sync::mpsc::channel;
-use std::time::Duration;
+use tauri::{Menu, MenuItem, Submenu};
+
+fn make_menu() -> Menu {
+  // https://docs.rs/tauri/1.0.0-beta.5/tauri/enum.MenuItem.html
+
+  let main_submenu = Submenu::new(
+    "Mnote",
+    Menu::new()
+      .add_native_item(MenuItem::Quit),
+  );
+
+  let edit_submenu = Submenu::new(
+    "Edit",
+    Menu::new()
+      .add_native_item(MenuItem::Undo)
+      .add_native_item(MenuItem::Redo)
+      .add_native_item(MenuItem::Separator)
+      .add_native_item(MenuItem::Cut)
+      .add_native_item(MenuItem::Copy)
+      .add_native_item(MenuItem::Paste)
+  );
+
+  Menu::new()
+    .add_submenu(main_submenu)
+    .add_submenu(edit_submenu)
+}
 
 fn main() {
-  tauri::Builder::default()
-    .invoke_handler(tauri::generate_handler![get_args, is_mac, is_windows, watcher_init])
+  let builder = tauri::Builder::default()
+    .invoke_handler(tauri::generate_handler![get_args, is_mac, is_windows, watcher_init]);
+
+  let builder = if cfg!(target_os = "macos") {
+    builder
+      .menu(make_menu())
+      .on_menu_event(move |event| {
+        let e: &str = event.menu_item_id();
+        event.window().emit("menu_event", e).unwrap();
+      })
+  } else {
+    builder
+  };
+
+  builder
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
 }
--- mnote-app/src/system.ts
+++ mnote-app/src/system.ts
@@ -1,5 +1,6 @@
 import { SystemCancelQuitHook, SystemInteropModule } from "mnote-core";
 import { invoke } from "@tauri-apps/api/tauri";
+import { listen } from "@tauri-apps/api/event";
 import { appWindow, getCurrent } from "@tauri-apps/api/window";
 
 export class System implements SystemInteropModule {
@@ -11,7 +12,7 @@ export class System implements SystemInteropModule {
     this.USES_CMD = await invoke("is_mac");
 
     // run before quit hooks
-    getCurrent().listen("close-requested", async () => {
+    await getCurrent().listen("close-requested", async () => {
       // https://github.com/tauri-apps/tauri/commit/8157a68af1d94de1b90a14aa44139bb123b3436b#
       // apparently even the example doesn;t work V
       /* if (await confirm('Are you sure?')) {
@@ -27,6 +28,10 @@ export class System implements SystemInteropModule {
       }
 
       await appWindow.close();
+    });
+
+    await listen("menu_event", (payload) => {
+      console.log("rust menu event", payload);
     });
 
     return this;
