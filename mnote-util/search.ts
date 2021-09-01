a8dffb4b9f7965b95ded8b70c2137f2c3ef73919
gfrancine <gracefrancine.t@gmail.com>
2021-09-01 14:39:12 +0700

    add search function

---
+++ mnote-util/search.ts
@@ -0,0 +1,20 @@
+// search
+
+export type Match = {
+  start: number,
+  end: number,
+}
+
+export function getMatches(value: string, searchTerm: string) {
+  const matches: Match[] = [];
+
+  while (true) {
+    const start = value.indexOf(searchTerm);
+    if (start === -1) break;
+    const end = start + searchTerm.length;
+    value = value.slice(end);
+    matches.push({ start, end });
+  }
+
+  return matches;
+}
