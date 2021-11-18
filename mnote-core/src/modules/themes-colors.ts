0d043f7ca81b384ea4e9cc971c894dd92ff41ca8
gfrancine <gracefrancine.t@gmail.com>
2021-11-18 11:36:01 +0700

    rename existing theme color names

--- mnote-core/src/modules/themes-colors.ts
+++ mnote-core/src/modules/themes-colors.ts
@@ -5,25 +5,25 @@ import OpenColor from "open-color";
 const gray = OpenColor.gray;
 
 export const light = {
-  "font-main": "Open Sans",
-  "font-monospace": "Fira Mono",
+  "main-font": "Open Sans",
+  "main-font-monospace": "Fira Mono",
 
-  "fg-main": gray[9],
-  "fg-secondary": gray[6],
-  "fg-dim": gray[5],
-  "fg-dimmer": gray[3],
+  "main-fg": gray[9],
+  "main-fg-secondary": gray[6],
+  "main-fg-dim": gray[5],
+  "main-main-fg-dimmer": gray[3],
 
-  "bg-main": "#fff",
-  "bg-secondary": gray[1],
+  "main-bg": "#fff",
+  "main-bg-secondary": gray[1],
 
   "shadow": "0px 2px 5px " + gray[7] + "32",
-  "overlay-bg": gray[7] + "32",
+  "main-overlay-bg": gray[7] + "32",
 
-  "border-main": gray[4],
-  "border-dim": gray[2],
+  "main-border": gray[4],
+  "main-border-dim": gray[2],
 
-  "sidebar-bg-main": gray[1],
-  "sidebar-fg-main": gray[9],
+  "sidebar-bg": gray[1],
+  "sidebar-fg": gray[9],
 
   "sidebar-item-icon": gray[6],
   "sidebar-item-hover-bg": gray[2],
@@ -34,41 +34,41 @@ export const light = {
   "sidebar-item-focused-icon": gray[6],
   "sidebar-filetree-border": gray[3],
 
-  "btn-main-fg": gray[9], // buttons, assuming background is bg-main
+  "btn-main-fg": gray[9], // buttons, assuming background is main-bg
   "btn-main-fg-hover": gray[9],
   "btn-main-bg": gray[2],
   "btn-main-bg-hover": gray[3],
 
   "btn-emphasis-fg": "#fff",
-  "btn-emphasis-fg-hover": "#fff",
+  "btn-emphasis-hover-fg": "#fff",
   "btn-emphasis-bg": gray[6],
-  "btn-emphasis-bg-hover": gray[7],
+  "btn-emphasis-hover-bg": gray[7],
 };
 
 // dark theme from my UI prototype
 
 export const dark = {
-  "font-main": "Open Sans",
-  "font-monospace": "Fira Mono",
+  "main-font": "Open Sans",
+  "main-font-monospace": "Fira Mono",
 
-  "fg-main": "#B0B6BB",
-  "fg-secondary": "#767E85",
-  "fg-dim": "#656A70",
-  "fg-dimmer": "#404345",
+  "main-fg": "#B0B6BB",
+  "main-fg-secondary": "#767E85",
+  "main-fg-dim": "#656A70",
+  "main-main-fg-dimmer": "#404345",
 
-  "bg-main": "#2B2D2E",
-  "bg-secondary": "#212324",
+  "main-bg": "#2B2D2E",
+  "main-bg-secondary": "#212324",
 
   "shadow": "0px 2px 5px " + "#111",
-  "overlay-bg": "#11111132",
+  "main-overlay-bg": "#11111132",
 
-  "border-main": "#111",
-  "border-dim": "#222",
+  "main-border": "#111",
+  "main-border-dim": "#222",
 
-  "sidebar-bg-main": "#212324",
+  "sidebar-bg": "#212324",
   "sidebar-bg-hover": "#2B2D2E",
-  "sidebar-fg-main": "#B0B6BB",
-  "sidebar-fg-secondary": "#767E85",
+  "sidebar-fg": "#B0B6BB",
+  "sidebar-main-fg-secondary": "#767E85",
 
   "sidebar-item-icon": "#767E85",
   "sidebar-item-hover-bg": "#2B2D2E",
@@ -79,48 +79,48 @@ export const dark = {
   "sidebar-item-focused-icon": "#767E85",
   "sidebar-filetree-border": "#404345",
 
-  "btn-main-fg": "#B0B6BB", // buttons, assuming background is bg-main
+  "btn-main-fg": "#B0B6BB", // buttons, assuming background is main-bg
   "btn-main-fg-hover": "#B0B6BB",
   "btn-main-bg": "#3F4446",
   "btn-main-bg-hover": "#4A5052",
 
   "btn-emphasis-fg": "#2B2D2E",
-  "btn-emphasis-fg-hover": "#2B2D2E",
+  "btn-emphasis-hover-fg": "#2B2D2E",
   "btn-emphasis-bg": "#B0B6BB",
-  "btn-emphasis-bg-hover": "#92989C",
+  "btn-emphasis-hover-bg": "#92989C",
 };
 
 /* oc dark theme
 
 const darkTheme = {
-  "font-main": "Lato",
-  "font-monospace": "Fira Mono",
+  "main-font": "Lato",
+  "main-font-monospace": "Fira Mono",
 
-  "fg-main": gray[2],
-  "fg-secondary": gray[4],
-  "fg-dim": gray[5],
+  "main-fg": gray[2],
+  "main-fg-secondary": gray[4],
+  "main-fg-dim": gray[5],
 
-  "bg-main": gray[8],
-  "bg-secondary": gray[9],
-  "fg-dimmer": gray[3],
+  "main-bg": gray[8],
+  "main-bg-secondary": gray[9],
+  "main-main-fg-dimmer": gray[3],
 
   "shadow": "0px 2px 5px " + "#111",
   "overlay": "#11111132",
 
-  "border-main": gray[9],
-  "border-dim": gray[9],
+  "main-border": gray[9],
+  "main-border-dim": gray[9],
 
-  "sidebar-bg-main": gray[9],
+  "sidebar-bg": gray[9],
   "sidebar-bg-hover": gray[7],
-  "sidebar-fg-main": gray[4],
+  "sidebar-fg": gray[4],
 
-  "btn-main-fg": gray[2], // buttons, assuming background is bg-main
+  "btn-main-fg": gray[2], // buttons, assuming background is main-bg
   "btn-main-fg-hover": gray[2],
   "btn-main-bg": gray[7],
   "btn-main-bg-hover": gray[6],
 
   "btn-emphasis-fg": gray[9],
-  "btn-emphasis-fg-hover": gray[9],
+  "btn-emphasis-hover-fg": gray[9],
   "btn-emphasis-bg": gray[3],
-  "btn-emphasis-bg-hover": gray[4],
+  "btn-emphasis-hover-bg": gray[4],
 }; */
