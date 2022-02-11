8e977193c9c165549ed7a368eddee239e180f23a
gfrancine <gracefrancine.t@gmail.com>
2022-02-11 17:03:04 +0700

    inputs module: add unregisterShortcut method, disable default shortcut prevention when focused on an input/textarea/contenteditable

--- mnote-core/modules/input.ts
+++ mnote-core/modules/input.ts
@@ -2,6 +2,17 @@ import { Mnote } from "..";
 import { LogModule } from "./log";
 const Mousetrap = require("mousetrap");
 
+// https://craig.is/killing/mice#api.stopCallback
+Mousetrap.prototype.stopCallback = () => {
+  // if the element has the class "mousetrap" then no need to stop
+  // if ((' ' + element.className + ' ').indexOf(' mousetrap ') > -1) {
+  //   return false;
+  // }
+  // stop for input, select, and textarea
+  // return element.tagName == 'INPUT' || element.tagName == 'SELECT' || element.tagName == 'TEXTAREA' || (element.contentEditable && element.contentEditable == 'true');
+  return false;
+};
+
 export class InputModule {
   private log: LogModule;
 
@@ -11,9 +22,19 @@ export class InputModule {
 
   registerShortcut(
     combinations: string[],
-    callback: (e: KeyboardEvent) => void | Promise<void>
+    callback: (e: KeyboardEvent) => void | Promise<void>,
+    event?: "keyup" | "keydown"
   ) {
     this.log.info("input: bind combinations", combinations);
-    Mousetrap.bind(combinations, callback);
+    Mousetrap.bind(combinations, callback, event);
+  }
+
+  unregisterShortcut(
+    combinations: string[],
+    callback: (e: KeyboardEvent) => void | Promise<void>,
+    event?: "keyup" | "keydown"
+  ) {
+    this.log.info("input: unbind combinations", combinations);
+    Mousetrap.unbind(combinations, callback, event);
   }
 }
