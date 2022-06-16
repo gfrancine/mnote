b04502ab4fa1d127976c7283aa0b7393c95d23f4
gfrancine <gracefrancine.t@gmail.com>
2022-06-16 15:00:17 +0700

    Revert "use native select list"
    This reverts commit 99dd0f0a218c6d26aa12f5ee266ac2d3da994e54.

--- mnote-components/react/dropdowns/Select.tsx
+++ mnote-components/react/dropdowns/Select.tsx
@@ -1,5 +1,6 @@
-import React, { useId, useState } from "react";
+import React, { useEffect, useMemo, useRef, useState } from "react";
 import * as Dropdown from "./Base";
+import { ChevronDown, ChevronUp } from "./icons";
 
 export default function Select<T extends string>({
   initialValue,
@@ -7,6 +8,9 @@ export default function Select<T extends string>({
   disabled,
   placeholder,
   onChange,
+  toggleClass,
+  menuClass,
+  itemClass,
   containerClass,
 }: {
   initialValue?: T;
@@ -17,42 +21,84 @@ export default function Select<T extends string>({
   disabled?: boolean;
   placeholder?: string;
   onChange?: (value: T) => unknown;
+  toggleClass?: string;
+  menuClass?: string;
+  itemClass?: string;
   containerClass?: string;
 }) {
-  const placeholderId = useId();
+  const optionsMap = useMemo(() => {
+    const map: Record<string, string> = {};
+    options.forEach(({ value, text }) => {
+      map[value] = text;
+    });
+    return map;
+  }, [options]);
 
+  const [expanded, setExpanded] = useState(false);
   const [selected, setSelected] = useState<T | null>(
     initialValue === undefined ? null : initialValue
   );
 
   const select = (value: T) => {
-    if (value === placeholderId) {
-      setSelected(null);
-      return;
-    }
     setSelected(value);
+    setExpanded(false);
     onChange?.(value);
   };
 
+  const menuRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const listener = (e: MouseEvent) => {
+      if (!menuRef.current) return;
+      if (!expanded) return;
+
+      const rect = menuRef.current.getBoundingClientRect();
+      const isWithin =
+        e.clientX > rect.x &&
+        e.clientX < rect.x + rect.width &&
+        e.clientY > rect.y &&
+        e.clientY < rect.y + rect.height;
+
+      if (!isWithin) {
+        setExpanded(false);
+      }
+    };
+
+    document.addEventListener("mousedown", listener);
+    return () => document.removeEventListener("mousedown", listener);
+  });
+
   return (
     <Dropdown.Container className={containerClass}>
-      <select
-        className={"dropdown-toggle " + (disabled ? " disabled " : " ")}
-        value={selected === null ? placeholderId : selected}
-        onChange={(e) => select(e.target.value as T)}
+      <Dropdown.Toggle
+        text={selected === null ? undefined : optionsMap[selected]}
+        placeholder={placeholder}
+        className={toggleClass}
         disabled={disabled}
-      >
-        {placeholder && (
-          <option key={placeholderId} value={placeholderId}>
-            {placeholder}
-          </option>
-        )}
+        onClick={() => {
+          if (disabled) return;
+          if (options.length < 1) return;
+          setExpanded(!expanded);
+        }}
+        getIcon={(fillClass, strokeClass) =>
+          expanded ? (
+            <ChevronUp strokeClass={strokeClass} fillClass={fillClass} />
+          ) : (
+            <ChevronDown strokeClass={strokeClass} fillClass={fillClass} />
+          )
+        }
+      />
+      <Dropdown.Menu visible={expanded} className={menuClass} menuRef={menuRef}>
         {options.map((option) => (
-          <option key={option.value} value={option.value}>
-            {option.text}
-          </option>
+          <Dropdown.Item
+            key={option.value}
+            className={itemClass}
+            text={option.text}
+            selected={selected === option.value}
+            onClick={() => select(option.value)}
+          />
         ))}
-      </select>
+      </Dropdown.Menu>
     </Dropdown.Container>
   );
 }
