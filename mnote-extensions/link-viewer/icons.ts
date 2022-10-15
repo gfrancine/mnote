a897f541e06a272072b626ce76fa188161a7d8a8
gfrancine <gracefrancine.t@gmail.com>
2022-10-15 16:49:47 +0700

    link viewer: add copy button

--- mnote-extensions/link-viewer/icons.ts
+++ mnote-extensions/link-viewer/icons.ts
@@ -6,18 +6,3 @@ export const linkIcon = (_fillClass: string, strokeClass: string) =>
     <path d="M208 352h-64a96 96 0 010-192h64M304 160h64a96 96 0 010 192h-64M163.29 256h187.42" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="36" class="${strokeClass}" />
   </svg>
 `);
-
-export const copyIcon = (_fillClass: string, strokeClass: string) =>
-  toHtml(`
-  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
-    <rect x="128" y="128" width="336" height="336" rx="57" ry="57" fill="none" stroke-linejoin="round" stroke-width="32" class="${strokeClass}" />
-    <path d="M383.5 128l.5-24a56.16 56.16 0 00-56-56H112a64.19 64.19 0 00-64 64v216a56.16 56.16 0 0056 56h24" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="32" class="${strokeClass}" />
-  </svg>
-`);
-
-export const openIcon = (_fillClass: string, strokeClass: string) =>
-  toHtml(`
-  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
-    <path d="M384 224v184a40 40 0 01-40 40H104a40 40 0 01-40-40V168a40 40 0 0140-40h167.48M336 64h112v112M224 288L440 72" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="32" class="${strokeClass}"/>
-  </svg>
-`);
--- mnote-extensions/link-viewer/index.ts
+++ mnote-extensions/link-viewer/index.ts
@@ -57,8 +57,21 @@ class LinkViewer implements Editor {
           </svg>
         </span>
       </a>
+      <button class="copy-button">
+        Copy Link
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" style="height:1em; width:1em;">
+          <rect x="128" y="128" width="336" height="336" rx="57" ry="57" fill="none" stroke-linejoin="round" stroke-width="32" stroke="currentColor" />
+          <path d="M383.5 128l.5-24a56.16 56.16 0 00-56-56H112a64.19 64.19 0 00-64 64v216a56.16 56.16 0 0056 56h24" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="32" stroke="currentColor" />
+        </svg>
+      </button>
     </div>
     `;
+
+    this.element
+      .querySelector(".copy-button")
+      ?.addEventListener("click", () => {
+        navigator.clipboard.writeText(link);
+      });
 
     return Promise.resolve();
   }
